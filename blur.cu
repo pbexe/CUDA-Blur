046cfce747db7b39a1e4be8fbac44eede619085f
Miles Budden <miles@budden.net>
2019-12-10 17:11:12 +0000

    All malloced

--- blur.cu
+++ blur.cu
@@ -17,16 +17,19 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
    }
 }
 
-
 __global__
 void blur(int *d_R, int *d_G, int *d_B, int *d_Rnew, int *d_Gnew, int *d_Bnew)
 {
+  // Get the X and y coords of the pixel for this thread
   int x = (blockIdx.x * blockDim.x) + threadIdx.x;
   int y = (blockIdx.y * blockDim.y) + threadIdx.y;
 
+  // Stop the thread if it is not part of the image
 	if (x >= IMAGE_WIDTH || y >= IMAGE_HEIGHT) {
       return;
   }
+
+  // Apply the box blur
   if (y != 0 && y != (IMAGE_HEIGHT-1) && x != 0 && x != (IMAGE_WIDTH-1)){
     d_Rnew[(IMAGE_WIDTH*y) + x] = (d_R[(IMAGE_WIDTH*y+1) + x]+d_R[(IMAGE_WIDTH*y-1) + x]+d_R[(IMAGE_WIDTH*y) + x+1]+d_R[(IMAGE_WIDTH*y) + x-1])/4;
     d_Gnew[(IMAGE_WIDTH*y) + x] = (d_G[(IMAGE_WIDTH*y+1) + x]+d_G[(IMAGE_WIDTH*y-1) + x]+d_G[(IMAGE_WIDTH*y) + x+1]+d_G[(IMAGE_WIDTH*y) + x-1])/4;
@@ -114,78 +117,82 @@ int main (int argc, const char * argv[]) {
 		}
 	}
 	fclose(fp);
-
-	nblurs = 10;
+  // Number of blur iterations
+	nblurs = 20;
+  // Start the timer
 	double t1=tim.tv_sec+(tim.tv_usec/1000000.0);
-
-	for(k=0;k<nblurs;k++){
-		int flat_R[IMAGE_WIDTH * IMAGE_HEIGHT];
-		int flat_G[IMAGE_WIDTH * IMAGE_HEIGHT];
-		int flat_B[IMAGE_WIDTH * IMAGE_HEIGHT];
-		for (int row=0;row<IMAGE_HEIGHT;row++){
-			for (int col=0;col<IMAGE_WIDTH;col++){
-				flat_R[IMAGE_WIDTH*row+col] = R[row][col];
-				flat_G[IMAGE_WIDTH*row+col] = G[row][col];
-				flat_B[IMAGE_WIDTH*row+col] = B[row][col];
-			}
-		}
-		int *d_R, *d_G, *d_B, *d_Rnew, *d_Gnew, *d_Bnew;
-		int size = sizeof(int) * IMAGE_WIDTH * IMAGE_HEIGHT;
-		cudaMalloc((void **)&d_R, size);
-		cudaMemcpy(d_R, flat_R, size, cudaMemcpyHostToDevice);
-		cudaMalloc((void **)&d_G, size);
-		cudaMemcpy(d_G, flat_G, size, cudaMemcpyHostToDevice);
-		cudaMalloc((void **)&d_B, size);
+  // The size of the 1D arrays for the GPU
+  int size = sizeof(int) * IMAGE_WIDTH * IMAGE_HEIGHT;
+  // Initialise the arrays to hold the flatened image
+  int *flat_R, *flat_G, *flat_B;
+  flat_R = (int *)malloc(size);
+  flat_G = (int *)malloc(size);
+  flat_B = (int *)malloc(size);
+  // Create pointers to GPU array locations
+  int *d_R, *d_G, *d_B, *d_Rnew, *d_Gnew, *d_Bnew;
+  // Define how many threads per block
+  int numBlocksY = ceil(IMAGE_HEIGHT/16.0);
+  int numBlocksX = ceil(IMAGE_WIDTH/16.0);
+  dim3 dimBlock(numBlocksX,numBlocksY);
+  // Define how many blocks per grid
+  dim3 dimGrid(16, 16);
+  // Allocate GPU mem for the 1D arrays
+  cudaMalloc((void **)&d_R, size);
+  cudaMalloc((void **)&d_G, size);
+  cudaMalloc((void **)&d_B, size);
+  cudaMalloc((void **)&d_Rnew, size);
+  cudaMalloc((void **)&d_Bnew, size);
+  cudaMalloc((void **)&d_Gnew, size);
+  // Pointers to handle the output
+  int *h_R, *h_G, *h_B;
+  h_R = (int *)malloc(size);
+  h_G = (int *)malloc(size);
+  h_B = (int *)malloc(size);
+  // Start the blur loop
+  for(k=0;k<nblurs;k++){
+    // Flatten the 2D arrays to make them easier to handle with CUDA
+  	for (int row=0;row<IMAGE_HEIGHT;row++){
+  		for (int col=0;col<IMAGE_WIDTH;col++){
+  			flat_R[IMAGE_WIDTH*row+col] = R[row][col];
+  			flat_G[IMAGE_WIDTH*row+col] = G[row][col];
+  			flat_B[IMAGE_WIDTH*row+col] = B[row][col];
+  		}
+  	}
+    // Copy these arrays to the GPU
+  	cudaMemcpy(d_R, flat_R, size, cudaMemcpyHostToDevice);
+  	cudaMemcpy(d_G, flat_G, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, flat_B, size, cudaMemcpyHostToDevice);
-    cudaMalloc((void **)&d_Rnew, size);
-		cudaMalloc((void **)&d_Bnew, size);
-    cudaMalloc((void **)&d_Gnew, size);
-		int numBlocksY = ceil(IMAGE_HEIGHT/16.0);
-		int numBlocksX = ceil(IMAGE_WIDTH/16.0);
-		dim3 dimBlock(numBlocksX,numBlocksY);
-		dim3 dimGrid(16, 16);
 
 
     // Punch it Chewie
-		blur<<<dimGrid, dimBlock>>>(d_R, d_G, d_B, d_Rnew, d_Gnew, d_Bnew);
+  	blur<<<dimGrid, dimBlock>>>(d_R, d_G, d_B, d_Rnew, d_Gnew, d_Bnew);
 
 
-		int *h_R, *h_G, *h_B;
-		h_R = (int *)malloc(size);
-    h_G = (int *)malloc(size);
-		h_B = (int *)malloc(size);
-		cudaMemcpy(h_R, d_Rnew, size, cudaMemcpyDeviceToHost);
-		cudaMemcpy(h_G, d_Gnew, size, cudaMemcpyDeviceToHost);
-		cudaMemcpy(h_B, d_Bnew, size, cudaMemcpyDeviceToHost);
-		cudaFree(d_R);
-		cudaFree(d_G);
-		cudaFree(d_B);
-		cudaFree(d_Rnew);
-		cudaFree(d_Gnew);
-		cudaFree(d_Bnew);
-		gpuErrchk( cudaPeekAtLastError() );
-		gpuErrchk( cudaDeviceSynchronize() );
-		for (int row=0;row<IMAGE_HEIGHT;row++){
-			for (int col=0;col<IMAGE_WIDTH;col++){
-				R[row][col] = h_R[IMAGE_WIDTH*row+col];
-				G[row][col] = h_G[IMAGE_WIDTH*row+col];
-				B[row][col] = h_B[IMAGE_WIDTH*row+col];
-			}
-		}
-
-		// for(row=0;row<rowsize;row++){
-		// 	for (col=0;col<colsize;col++){
-		// 	    R[row][col] = Rnew[row][col];
-		// 	    G[row][col] = Gnew[row][col];
-		// 	    B[row][col] = Bnew[row][col];
-		// 	}
-		// }
-	}
-
+  	cudaMemcpy(h_R, d_Rnew, size, cudaMemcpyDeviceToHost);
+  	cudaMemcpy(h_G, d_Gnew, size, cudaMemcpyDeviceToHost);
+  	cudaMemcpy(h_B, d_Bnew, size, cudaMemcpyDeviceToHost);
+  	gpuErrchk( cudaPeekAtLastError() );
+  	gpuErrchk( cudaDeviceSynchronize() );
+  	for (int row=0;row<IMAGE_HEIGHT;row++){
+  		for (int col=0;col<IMAGE_WIDTH;col++){
+  			R[row][col] = h_R[IMAGE_WIDTH*row+col];
+  			G[row][col] = h_G[IMAGE_WIDTH*row+col];
+  			B[row][col] = h_B[IMAGE_WIDTH*row+col];
+  		}
+  	}
+  }
+  cudaFree(d_R);
+  cudaFree(d_G);
+  cudaFree(d_B);
+  cudaFree(d_Rnew);
+  cudaFree(d_Gnew);
+  cudaFree(d_Bnew);
+  free(h_R);
+  free(h_G);
+  free(h_B);
 	gettimeofday(&tim, NULL);
 	double t2=tim.tv_sec+(tim.tv_usec/1000000.0);
 	printf("%.6lf seconds elapsed\n", t2-t1);
-
 	fout= fopen("DavidBlur.ps", "w");
 	for (k=0;k<nlines;k++) fprintf(fout,"\n%s", lines[k]);
 	fprintf(fout,"\n");
