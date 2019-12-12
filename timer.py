844511a278b6ad41be523d2bf569ae5e80068faf
Miles Budden <miles@budden.net>
2019-12-12 12:03:19 +0000

    New stats

--- timer.py
+++ timer.py
@@ -13,13 +13,13 @@ def time_prog(blurs):
 
 os.system("nvcc -o blur blur.cu")
 with open('results.csv', 'w', newline='') as csvfile:
-    fieldnames = ['blurs', 'time']
+    fieldnames = ['blurs', 'time', 'std']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
-    for i in range(10,310, 10):
+    for i in range(10,510, 10):
         times = []
         for _ in range(50):
             times.append(float(time_prog(i)))
-        print("Blurs:", i, statistics.stdev(times), "MEAN:", statistics.median(times))
-        writer.writerow({'blurs': i, 'time': statistics.median(times)})
+        print("Blurs:", i, statistics.stdev(times), "MEDIAN:", statistics.median(times))
+        writer.writerow({'blurs': i, 'time': statistics.median(times), 'std':statistics.stdev(times)})
